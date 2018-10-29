3cc0fe2babd639841e9801b1ee9560fd05c3ea8f
literale <43133944+literale@users.noreply.github.com>
2018-10-29 13:05:33 +0600

    Добавление отработанных часов
    Добавлено считывание из файла и добавление к экземплярам-работникам отработанных ими часов

--- hw06_hard.py
+++ hw06_hard.py
@@ -12,6 +12,7 @@
 # каждый работник получал строку из файла
 class Worker(object):
     payment = 0
+    done_hour = 0
     def __init__(self, init_str):
         """Constructor"""
         list_init_str = init_str.split()
@@ -21,14 +22,19 @@ class Worker(object):
         self.post = list_init_str[3]
         self.norm_hour = list_init_str[4]
         pass
-    
+
     def set_payment(self, payment):
         self.payment = payment
         pass
 
+    def set_done_hour(self, done_hour):
+        self.done_hour = done_hour
+        pass
+
     def __str__(self):
-        return "Имя: {0} Фамилия: {1} Зарплата: {2} Должность: {3} Норма часов : {4} Зарплата за месяц : {5}"\
-            .format(self.name, self.second_name, self.salary, self.post, self.norm_hour, self.payment)
+        return "Имя: {0} Фамилия: {1} Зарплата: {2} Должность: {3} Норма часов : {4} " \
+               "Зарплата за месяц : {5} Отработано часов : {6}"\
+            .format(self.name, self.second_name, self.salary, self.post, self.norm_hour, self.payment, self.done_hour)
 
 
 
@@ -37,13 +43,27 @@ class Worker(object):
 def main():
     workers = []
     count = 0
-    #инициализируем работников
+    # инициализируем работников
     with open('data\\workers', 'r', encoding="utf-8") as f:
         for line in f:
-            #print(line)
+            # print(line)
             if count > 0:
                 workers.append(Worker(line))
             count += 1
+
+    # Добавляем отработанные часы
+    with open('data\\hours_of', 'r', encoding="utf-8") as f:
+        for line in f:
+            # print(line)
+            line_list = line.split()
+            if count > 0:
+                for w in workers:
+                    if w.name == line_list[0] and w.second_name == line_list[1]:
+                        w.done_hour = line_list[2]
+            count += 1
+
+
+
 
     for w in workers:
         print(w)
